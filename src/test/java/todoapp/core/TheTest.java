fb51dd459e1e8ad9be891194e61f15f90857939e
Gregor Riegler <gregor.riegler@ebcont.com>
2019-07-18 11:50:29 +0200

    returns tasks

--- src/test/java/todoapp/core/TheTest.java
+++ src/test/java/todoapp/core/TheTest.java
@@ -2,28 +2,63 @@ package todoapp.core;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TheTest {
 
     @Test
     void shouldListEmptyTasks() {
-        var the = new The();
+        var the = new The(new TaskList());
         var tasks = the.listTasks();
         assertThat(tasks.isEmpty()).isTrue();
+    }
+
+    @Test
+    void shouldListSingleTask() {
+        var givenTask = new Task();
+        var currentTasks = new TaskList(givenTask);
+
+        var the = new The(currentTasks);
+        var tasks = the.listTasks();
+
+        assertThat(tasks.isEmpty()).isFalse();
+        assertThat(tasks.getFirstTask()).isEqualTo(givenTask);
     }
 
 }
 
 class The {
+    private final TaskList tasks;
+
+    public The(TaskList tasks) {
+        this.tasks = tasks;
+    }
+
     public TaskList listTasks() {
-        return new TaskList();
+        return tasks;
     }
 }
 
 class TaskList {
 
-    public boolean isEmpty() {
-        return true;
+    private final List<Task> tasks;
+
+    public TaskList(Task... tasks) {
+        this.tasks = Arrays.asList(tasks);
     }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public Task getFirstTask() {
+        return tasks.get(0);
+    }
+}
+
+class Task {
+
 }

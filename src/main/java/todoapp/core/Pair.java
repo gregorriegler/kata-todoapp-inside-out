4ad72e9aa18cc2938ffe58a13556cbd43c3b64ff
Peter Kofler <peter.kofler@code-cop.org>
2019-07-21 14:33:30 +0200

    Fix generics warning.

--- src/main/java/todoapp/core/Pair.java
+++ src/main/java/todoapp/core/Pair.java
@@ -5,7 +5,7 @@ public class Pair<F, S> {
     public final F first;
     public final S second;
 
-    public static <F, S> Pair of(F first, S second) {
+    public static <F, S> Pair<F, S> of(F first, S second) {
         return new Pair<>(first, second);
     }
 
